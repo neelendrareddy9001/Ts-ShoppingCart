24fedccf207ab7abff30c0e05332ca2935a1c915
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-08 22:42:22 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import {Container,Nav,Button, Navbar as NavbarBs} from 'react-bootstrap'
 import { NavLink} from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 export function Navbar () {
+    const {openCart , cartQuantity} = useShoppingCart()
+    
     return (
         <NavbarBs sticky="top" className='bg-white shadow-sm mb-3'>
             <Container>
